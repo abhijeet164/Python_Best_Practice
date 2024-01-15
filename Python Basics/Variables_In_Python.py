587c4778bf4f9eedb5e27a1c1aa5f964535810a8
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-15 12:47:11 +0530

    Variable Assignment

---
+++ Python Basics/Variables_In_Python.py
@@ -0,0 +1,12 @@
+""" A VAriable is Nothing but a reserved memory Location to Store Value
+     Variables are used tostore data
+     Memory is Allocated when Value is Stored in Variable
+     Every Variable must have Some data type"""
+# <Variable Name> = <Value Assigned>
+
+a = 10
+b = "Computer"
+print(a)          #10
+print(type(a))    # <class 'int'>
+print(b)          # Computer
+print(type(b))    #<class 'str'>
