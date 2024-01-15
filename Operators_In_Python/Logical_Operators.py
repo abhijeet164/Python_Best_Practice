d2d670bb5e26f9cadd2b689f37aaf4e498b4c825
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-16 00:08:50 +0530

    All About Logical Opreators

---
+++ Operators_In_Python/Logical_Operators.py
@@ -0,0 +1,21 @@
+# The Following are Logical Operators are [ and , or ,not ] These are KeyWords Not  Symbols
+# NOTE : Don't get Confused with Logical Operators and Bitwise Operators
+#        We Always use Logical Opreator with boolean Values
+#        Logical Operators always returns boolean value
+
+#       |  a    |  b    | a and b | a or b | not a |
+#       | True  | True  |  True   | True   | False |
+#       | True  | False |  False  | True   | False |
+#       | False | True  |  False  | True   | True  |
+#       | False | False |  False  | False  | True  |
+
+# Logical and = Both Should be True
+# Logical  or = Atleast One Should be True
+# Logical not =  Negation Opreator if True than O/p is False And Viceversa
+
+a = True
+b = False
+
+print(a and b)  # False
+print(a or b)  # True
+print(not a)  # False
