34687f04209d8a3f2ccff6bea563c02d91e37164
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-15 23:31:04 +0530

    All About Relational Opreators

---
+++ Operators_In_Python/Relational_Operators.py
@@ -0,0 +1,12 @@
+# Relational / Comparison Operator
+# There are six type of Relational / Comparison Operator (< ,> ,>= ,<=, ==,!=)
+# Relational / Comparison Operator always Return Boolean Value i.e True /False
+
+a = 5
+b = 2
+print(a > b)    # a is greater than b hence  O/p  =  True
+print(a < b)    # a is smaller than b hence O/p   =  False
+print(a >= b)   # a is greater than or equal to b =  True
+print(a <= b)   # a is smaller than or equal to b =  False
+print(a != b)   # a is not equal to b             =  True
+print(a == b)   # a is equal to b                 =  False
