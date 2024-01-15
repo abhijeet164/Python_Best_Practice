5bb89badba8a3e32563bd8f03eeecbb7ee567c29
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-15 12:22:10 +0530

    All Data Types

--- Python Basics/Data_Types.py
+++ Python Basics/Data_Types.py
@@ -1,1 +1,20 @@
-str_a= "Hello World"
+a = 10
+b = 20.5
+c = 'Abhijeet'
+c1 = "A"
+d = 10+0j
+e = True
+f = False
+g = 0
+i = 1
+# Printing the  Data types of above Variable
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(c1))
+print(type(d))
+print(type(e))
+print(type(f))
+print(bool(g))
+print(type(g))
+print(bool(i))
