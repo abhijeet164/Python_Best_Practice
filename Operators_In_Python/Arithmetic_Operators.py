cf939910bdd19153f6850ed715cdead7cdb7f231
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-15 20:30:49 +0530

    All About Arithmatic Operators

--- Operators_In_Python/Arithmetic_Operators.py
+++ Operators_In_Python/Arithmetic_Operators.py
@@ -8,7 +8,7 @@ b = 2
 print(a+b)    # Yeilds 7     Addition
 print(a-b)    # Yeilds 3     Substraction
 print(a*b)    # Yeilds 10    Multipication
-print(a/b)    # Yeilds 2.5   Diovision
+print(a/b)    # Yeilds 2.5   Division
 print(a//b)   # Yeilds 2     Yeilds Quotient After Discarding Fractional Value
 print(a%b)    # Yeilds 1     Yeilds Remainder
 print(a**b)   # Yeilds 25    Yeilds exponentiation
