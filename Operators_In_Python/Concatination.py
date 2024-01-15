10fff5c4327b229b9d3491ccc4c12bb222f3196a
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-16 00:47:17 +0530

    All About Concatination

---
+++ Operators_In_Python/Concatination.py
@@ -0,0 +1,15 @@
+""" Concationation : Concatenation, broadly speaking, is the linking or joining
+of two things to achieve a certain result. In computer programming it links
+ two characters or character strings together to create a phrase or compound word. """
+
+print(10+10)                  # Gives  Addition of Integers = 20
+print(10.2 + 12.5)            # Gives  Addition of Float    = 22.7
+print(10.2 + 12)              # Gives  Addition of Float      = 22.2
+print('Welcome' + 'Python')   # Gives WelcomePython
+print(True + 5)               # 6
+print(False + 5)              # 5
+print(True + True)            # 2
+print(False + False)          # 0
+
+# True = 1 And False = 0
+
