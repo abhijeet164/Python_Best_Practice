54710072348e0c697c41d20822879130fc0af490
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-15 10:52:57 +0530

    All About Comments

---
+++ Python Basics/Comments_in_Python.py
@@ -0,0 +1,10 @@
+# Comments are used to write Description about the Appln logic to understand logic Easily
+# Comments are not Executable Code
+# python will ignore comments in Run Time
+
+print("Hello World")
+
+# This is single line comment
+
+"""this is multiline comments 
+in python"""
