f32b42bd629bf89eaeea07d3663d3da99dc3c6ce
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-16 11:44:10 +0530

    All About Formatting to Console

---
+++ KeyWords/Formatting_Output.py
@@ -0,0 +1,38 @@
+name = 'Abhijeet'
+age = 30
+salary = 5000
+
+# Approach 1
+print(name)   #Abhijeet
+print(age)    # 30
+print(salary)  # 500
+
+# Approach 2
+print(name,age,salary) # Abhijeet 30 5000 Decleard in one Statemaent
+
+# Approach 3 : Adding Comments in OutPut
+print('Name is:',name)     # Name is: Abhijeet
+print('Age is:',age)       #Age is: 30
+print('Salary :',salary)   #Salary : 5000
+
+#Approach 3 By Using % Symbol
+print("Name is:%s Age is:%d Salary is:%g" %(name,age,salary)) #Name is:Abhijeet Age is:30 Salary is:5000
+
+#Approach 4 By Using {} Braceses
+print("Name is:{} Age is:{}  Salary is:{}".format(name,age,salary))
+print("Age is:{} Name is:{}   Salary is:{}".format(name,age,salary)) #Age is:Abhijeet Name is:30   Salary is:5000   Order Matters
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
