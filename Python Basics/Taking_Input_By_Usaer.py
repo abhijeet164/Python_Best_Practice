a0c4f5ae8fffa2ab4adb7c562e19850b0f22e892
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-16 13:08:42 +0530

    All About Taking Input From User

---
+++ Python Basics/Taking_Input_By_Usaer.py
@@ -0,0 +1,27 @@
+# Approcah 1
+a = input("Enter First Number")
+b = input("Enter Second Number")
+print(a, type(a))  # <class 'str'>
+print(b, type(b))
+
+# approach 2
+num1 = int(input("Enter First Number"))  # type conversion <class 'int'>
+num2 = int(input("Enter Second Number"))
+print(num1, type(num1))
+print(num2, type(num2))
+
+# approach 3
+numA = input("Enter First Number")  # type conversion <class 'int'>
+numB = input("Enter Second Number")
+print(int(numA) + int(numB))
+
+# Approach 4
+numA1 = input("Enter First Decimal Number")  # type conversion <class 'int'>
+numB1 = input("Enter Second Decimal Number")
+print(type(numA1))
+print(type(numB1))
+
+print(float(numA1))  # Type Conversion String to Float
+print(float(numB1))
+
+print(float(numA1) + float(numB1))  # 3.7
