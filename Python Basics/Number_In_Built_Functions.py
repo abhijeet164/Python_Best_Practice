3ec43158e4c92d626a85b322e3f0b53ba104612b
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-17 14:06:42 +0530

    Number in built Function

--- Python Basics/Number_In_Built_Functions.py
+++ Python Basics/Number_In_Built_Functions.py
@@ -5,5 +5,5 @@ print(type(num2))  # <class 'float'>
 
 num3 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 print(type(num3))  # <class 'list'>
-print(max(num3))  # 100
-print(min(num3))  # 10
+print(max(num3))  # 100 Prints max value
+print(min(num3))  # 10 Prints min Value
