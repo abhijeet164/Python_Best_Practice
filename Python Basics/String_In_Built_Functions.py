a3446c6eab8a04d243f397d399cfe6b5e437b00d
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-17 17:39:50 +0530

    String Built in Fuctions

--- Python Basics/String_In_Built_Functions.py
+++ Python Basics/String_In_Built_Functions.py
@@ -40,9 +40,47 @@ print(str_A[-1])  # Yields (Space)
 
 # Accessing Sub_String of a String
 str_A = 'Abhijeet Rane'
-print(str_A[1:5]) # Extract from Specified value of Index Position => bhij
+print(str_A[1:5])  # Extract from Specified value of Index Position => bhij
 print(str_A[:5])
 print(str_A[1:3])
 print(str_A[-5:-1])
 print('Sclicinhg of String')
 
+#ord() and chr() Returns ASCII Value and viceversa
+print(ord('A')) #==>65
+print(chr(65))  #==> A
+
+#prints min() , max() ,len()
+print(min('abc'))  #a
+print(max('abc'))  #c
+print(len('abhijeet')) #8
+
+#MebershipOperator : in & not in Operator
+str1="Welcome to pune"
+print("come" in str1) #if there returns boolean value == True
+print("Wel"in str1)#True
+print("abhi"not in str1)#True
+print("pune" not in str1) #False
+
+#String Comparison  : compares on alphabetical Order
+print("tim"== "tie") #False
+print("free"!= "freedom") #True
+print("arrow">"arron") #True
+print("abhi"<"abhijeet") #True
+print("right">="left") #True
+print("Yellow"<="fellow") #True
+print("abc">"") #False
+
+
+# Testing String OR Type of Character present in String
+s="Welcome To Kolhapur"
+print(s.isalnum())
+print(s.isalpha())
+print(s.isascii())
+
+# finding Substring  Returns Boolean Value
+st = "Welcome to pune"
+print(st.endswith("w"))
+print(st.endswith("e"))
+print(st.startswith("welcome"))
+print(st.startswith("to"))
