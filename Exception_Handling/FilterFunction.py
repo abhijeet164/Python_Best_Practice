270287633ad081889b4b980fd64c04ca939c9daf
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-24 19:49:01 +0530

    Filter Function

--- Exception_Handling/FilterFunction.py
+++ Exception_Handling/FilterFunction.py
@@ -1,4 +1,5 @@
-# The filter() Function Returns an Iterator where the items are filtered through a function to Test ig the item is accepted or not
+# The filter() Function Returns an Iterator where the items are filtered through a function to Test ig the item is
+# accepted or not
 
 # Ex 1) Filter Function Without using Lambada Function
 
@@ -18,7 +19,7 @@ adult = list(filter(myFunc, ages))
 for x in adult:
     print(x)
 
-# Ex 2 Filter() Function with Lamabada Excrepisson
+# Ex 2 Filter() Function with Lambada Expression
 
 ages = [5, 12, 25, 18, 16, 32, 24]
 
