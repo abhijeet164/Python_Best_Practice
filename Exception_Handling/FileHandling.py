f03a4f8f7f0be55d8c0fc455b0b4ad078986e91f
Abhijeet Rane <abhijeetranesrk99@gmail.com>
2024-01-24 17:41:13 +0530

    File Handling

--- Exception_Handling/FileHandling.py
+++ Exception_Handling/FileHandling.py
@@ -1,25 +1,25 @@
 # wrinting  text  inside a text file
-#
-# file=open("C:/Python Notes/anuradha.txt",'w')
-# file.write("Hello Anuradha\n")
-# file.write("Hello Radha\n")
-# file.write("Hello Nilam\n")
-# file.write("Hello Nilu\n")
-# file.close()
-# print("Program Completed")
+
+file=open("C:\ISTQB Agile\myfile.txt",'w')
+file.write("Hello Anuradha\n")
+file.write("Hello Radha\n")
+file.write("Hello Nilam\n")
+file.write("Hello Nilu\n")
+file.close()
+print("Program Completed")
 
 
 
 # Read the data on console
-# file=open("C:/Python Notes/anuradha.txt",'r')
-# s=file.read()
-# print(s)
+file=open("C:/Python Notes/anuradha.txt",'r')
+s=file.read()
+print(s)
 
 # Appending data to file
 file=open("C:/Python Notes/anuradha.txt",'a')
-file.write("My Love Anu \n")
-file.write("My Love Radha\n")
-file.write("My Love Aisharya \n")
+file.write("Good Morning Anu \n")
+file.write("Good Morning Radha\n")
+file.write("Good MorningAisharya \n")
 print("Execution Coplitade")
 
 
